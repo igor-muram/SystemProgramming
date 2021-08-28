35bd2f68fe881f044054b6b47b5c9bafba0d2f5d
IgorMuramshchikov <igor.muram@gmail.com>
2021-08-28 19:35:43 +0700

    Minor fixes for Process Interaction

--- ProcessInteraction/main.c
+++ ProcessInteraction/main.c
@@ -15,97 +15,101 @@
 
 typedef struct message
 {
-    long mtype;
-    char mtext[SIZE];
+	long mtype;
+	char mtext[SIZE];
 } msg;
 
 const int mtype = 1;
 
-int process_args(char *arg)
+int process_args(char* arg)
 {
-    int length = strlen(arg);
+	int length = strlen(arg);
 
-    for (int i = 0; i < length; i++)
-        if (!isdigit(arg[i]))
-            return -1;
+	for (int i = 0; i < length; i++)
+		if (!isdigit(arg[i]))
+			return -1;
 
-    return atoi(arg);
+	return atoi(arg);
 }
 
-int main(int argc, char **argv)
+int main(int argc, char** argv)
 {
-    // Process argument
-    if (argc != 2)
-    {
-        errno = EINVAL;
-        perror("Wrong number of arguments");
-        return errno;
-    }
-
-    int K = process_args(argv[1]);
-    if (K == -1)
-    {
-        errno = EINVAL;
-        perror("Argument must be a number");
-        return errno;
-    }
-
-    printf("Argument equals %d\n", K);
-
-    // Create message queue
-    int msgqid = msgget(0, IPC_CREAT | 0666);
-    if (msgqid < 0)
-    {
-        perror("msgget");
-        return errno;
-    }
-
-    // Create K processes
-    for (int i = 0; i < K; i++)
-    {
-        int pid = fork();
-        if (pid == -1)
-        {
-            perror("fork");
-            return errno;
-        }
-
-        if (pid == 0)
-        {
-            int child = getpid();
-            msg m;
-            m.mtype = mtype;
-            int data = i % 7;
-            sprintf(m.mtext, "%d", data);
-            printf("child process: %d, data to queue: %s\n", child,  m.mtext);
-            if(msgsnd(msgqid, (void *)&m, strlen(m.mtext) + 1, IPC_NOWAIT))
-	    {
-		perror("msgsnd");
-                exit(errno);
-            }
-            exit(0);
-        }
-    }
-
-    // Create table
-    struct table t;
-    init(&t);
-
-    for (int i = 0; i < K; i++)
-    {
-        msg m;
-        if(msgrcv(msgqid, (void *)&m, SIZE, mtype, 0) < 0)
+	// Process argument
+	if (argc != 2)
 	{
-		perror("msgrcv");
+		errno = EINVAL;
+		perror("Wrong number of arguments");
 		return errno;
 	}
 
-        char *data = m.mtext;
-        printf("parent process, data from queue: %s\n", data);
-        if (!t.contains(&t, data))
-            t.put(&t, data);
-    }
+	int K = process_args(argv[1]);
+	if (K == -1)
+	{
+		errno = EINVAL;
+		perror("Argument must be a number");
+		return errno;
+	}
 
-    t.print(&t);
-    return 0;
+	// Create message queue
+	int msgqid = msgget(0, IPC_CREAT | 0666);
+	if (msgqid < 0)
+	{
+		perror("msgget");
+		return errno;
+	}
+
+	// Create K processes
+	for (int i = 0; i < K; i++)
+	{
+		int pid = fork();
+		if (pid == -1)
+		{
+			perror("fork");
+			return errno;
+		}
+
+		if (pid == 0)
+		{
+			// Create message
+			int child = getpid();
+			msg m;
+			m.mtype = mtype;
+			int data = i % 7;
+			sprintf(m.mtext, "%d", data);
+			printf("child process: %d, data to queue: %s\n", child, m.mtext);
+			// Send message to queue
+			if (msgsnd(msgqid, (void*)&m, SIZE, IPC_NOWAIT))
+			{
+				perror("msgsnd");
+				exit(errno);
+			}
+			exit(0);
+		}
+	}
+
+	// Create table
+	struct table t;
+	init(&t);
+
+	for (int i = 0; i < K; i++)
+	{
+		msg m;
+		// Receive massage from queue
+		if (msgrcv(msgqid, (void*)&m, SIZE, mtype, 0) < 0)
+		{
+			perror("msgrcv");
+			return errno;
+		}
+
+		char* data = m.mtext;
+		printf("parent process, data from queue: %s\n", data);
+
+		//put data to the table
+		if (!t.contains(&t, data))
+			t.put(&t, data);
+	}
+
+	// Print table	
+	t.print(&t);
+	return 0;
 }
